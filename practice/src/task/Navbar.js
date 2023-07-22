03ba23e4dbe587253a94e08c27d451097dc881a6
jyotinalwade <jyotinalwade13@gmail.com>
2023-07-22 17:00:57 +0530

    React Forms

---
+++ practice/src/task/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <ul className='links'>
+            <Link to="/" style={{marginLeft:"0px"}}>Task1</Link>
+            <Link to="/task2">Task2</Link>c
+            <Link to="/task3">Task3</Link>
+            <Link to="/task4">Task4</Link>
+        </ul>
+    </div>
+  )
+}
+export default Navbar
